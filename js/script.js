0b01cf6293691680237228dedd466e1edd218742
Maksym Serhieiev <84066632+maksimsergeev97@users.noreply.github.com>
2022-09-26 14:07:07 +0300

    Add files via upload
    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,55 +1,68 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menuBtn = document.querySelector('.menu__burger__img'),
-          menuBtnClose = document.querySelector('.menu__burger__close'),
-          menuBurger = document.querySelector('.menu__burger');
-
-
-    function showMenuBurger () {
-        menuBurger.classList.add('menu__burger__active');
-        menuBtn.style.display = "none";
+window.addEventListener("DOMContentLoaded", () => {
+    const menuBtn = document.querySelector(".menu__burger__img"),
+      menuBtnClose = document.querySelector(".menu__burger__close"),
+      menuBurger = document.querySelector(".menu__burger");
+  
+    function showMenuBurger() {
+      menuBurger.classList.add("menu__burger__active");
+      menuBtn.classList.add("hide_element");
+    }
+  
+    function hideMenuBurger() {
+      menuBurger.classList.remove("menu__burger__active");
+      menuBtn.classList.remove("hide_element");
+    }
+  
+    menuBtn.addEventListener("click", showMenuBurger);
+  
+    menuBtnClose.addEventListener("click", hideMenuBurger);
+  
+    function changeContent(item) {
+      const mainBlock = document.querySelector(".main");
+      mainBlock.innerHTML = `<img src="${item.img}" alt="${item.altText}" class="main__img">
+                                 <h2 class="title">${item.title}</h2>
+                                 <p class="subtitle">inhabitants of the planet: ${item.subtitle}</p>
+                                 <p class="descr">${item.descr}</p>`;
     }
 
-    function hideMenuBurger () {
-        menuBurger.classList.remove('menu__burger__active');
-        menuBtn.style.display = "block";
-    }
-
-    menuBtn.addEventListener('click', () => {
-        showMenuBurger();
-    })
-
-    menuBtnClose.addEventListener('click', () => {
-        hideMenuBurger()
-    })
-
-    function addEventForItem (selector) {
-        const links = document.querySelectorAll(selector);
-        const mainBlock = document.querySelector('.main');
-        links.forEach((item, i) => {
-            item.addEventListener('click', () => {
-                fetch('js/base.json')
-                    .then(response => {
-                        return response.json();
-                    })
-                    .then(data => {
-                       changeContent(data[i]);
-                });
-                mainBlock.scrollTop = 0;
-                hideMenuBurger();
+    function getData (item) {
+      fetch("js/base.json")
+            .then((response) => {
+              return response.json();
             })
-        })
+            .then((data) => {
+              changeContent(data[item]);
+            }).catch((error) => {
+              console.log(error);
+            })
     }
 
-    addEventForItem ('.menu__item');
-    addEventForItem ('.menu__burger__item');
+    function renderContent (selector, target) {
+      const items = document.querySelectorAll(selector);
+      items.forEach((item,i) => {
+        if (target == item && target.tagName == "LI") {
+          getData(i);
+          hideMenuBurger();
+        } else {
+          return;
+        };
+      });
+    };
 
-    function changeContent (item) {
-        const mainBlock = document.querySelector('.main');
-        mainBlock.innerHTML = `<img src="${item.img}" alt="${item.altText}" class="main__img">
-                               <h2 class="title">${item.title}</h2>
-                               <p class="subtitle">inhabitants of the planet: ${item.subtitle}</p>
-                               <p class="descr">${item.descr}</p>`
+    function addEventClick (menuSelector, itemsSelector) {
+      const menu = document.querySelector(menuSelector)
+      menu.addEventListener('click', (e) => {
+        const target = e.target;
+        renderContent(itemsSelector, target);
+      });
     }
 
+    addEventClick(".menu__list", ".menu__item");
+    addEventClick(".menu__burger__list", ".menu__burger__item")
 
-})
+  });
+
+
+
+
+  
